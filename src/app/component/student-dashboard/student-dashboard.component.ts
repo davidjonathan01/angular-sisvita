824411a252f2fe56aa866d289a73f460335ee351
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-06-27 12:35:24 -0500

    version 2.0.5

--- src/app/component/student-dashboard/student-dashboard.component.ts
+++ src/app/component/student-dashboard/student-dashboard.component.ts
@@ -8,13 +8,14 @@ import { InboxComponent } from "./inbox/inbox.component";
 import { ForoComponent } from "./foro/foro.component";
 import { TalleresComponent } from "./talleres/talleres.component";
 import { RecursosComponent } from "./recursos/recursos.component";
+import { PerfilComponent } from "./perfil/perfil.component";
 
 @Component({
     selector: 'app-student-dashboard',
     standalone: true,
     templateUrl: './student-dashboard.component.html',
     styleUrl: './student-dashboard.component.css',
-    imports: [CommonModule, RealizarTestComponent, ResultsComponent, SolicitarCitaComponent, CalendarioCitasComponent, InboxComponent, ForoComponent, TalleresComponent, RecursosComponent]
+    imports: [CommonModule, RealizarTestComponent, ResultsComponent, SolicitarCitaComponent, CalendarioCitasComponent, InboxComponent, ForoComponent, TalleresComponent, RecursosComponent, PerfilComponent]
 })
 export class StudentDashboardComponent {
   activeForm: string = '';
