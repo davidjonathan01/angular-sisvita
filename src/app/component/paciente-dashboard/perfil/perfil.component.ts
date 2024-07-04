e32f1c664b96b1018c54f6765dbd171510624abf
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-07-03 23:03:37 -0500

    avances de perfil de paciente

--- src/app/component/paciente-dashboard/perfil/perfil.component.ts
+++ src/app/component/paciente-dashboard/perfil/perfil.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Paciente } from '../../../model/paciente';
 
 @Component({
   selector: 'app-perfil',
@@ -8,6 +10,26 @@ import { Component } from '@angular/core';
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+  paciente?: Paciente;
+  readonly BASE_URL: string | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.cargarDatosPaciente();
+  }
+
+  cargarDatosPaciente(): void {
+    // Reemplaza 'url-a-tu-backend' con la URL de tu backend
+    this.http.get<Paciente>(`${this.BASE_URL}/paciente_routes/get_paciente/{id}`).subscribe({
+      next: (datosPaciente: Paciente) => {
+        this.paciente = datosPaciente;
+      },
+      error: (error) => {
+        console.error('Hubo un error al obtener los datos del paciente:', error);
+      }
+    });
+  }
 
 }
