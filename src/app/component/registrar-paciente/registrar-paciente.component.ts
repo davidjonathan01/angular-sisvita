198c0ca80cdf60aeed72b589f5f92f9c1143c033
davidjonathan01 <davidfernandezdavila01@gmail.com>
2024-07-04 04:17:25 -0500

    version7.3

--- src/app/component/registrar-paciente/registrar-paciente.component.ts
+++ src/app/component/registrar-paciente/registrar-paciente.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
 import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -14,6 +15,8 @@ import { UsuarioResponse } from '../../model/usuario-response';
 import { AuthService } from '../../services/auth.service';
 import { PacienteService } from '../../services/paciente.service';
 
+import { RegistrarPacienteService } from '../../services/registrar-paciente.service';
+
 
 
 @Component({
@@ -23,10 +26,10 @@ import { PacienteService } from '../../services/paciente.service';
   templateUrl: './registrar-paciente.component.html',
   styleUrl: './registrar-paciente.component.css'
 })
-export class RegistrarPacienteComponent {
-  pacienteArray: Paciente[] = [];
+export class RegistrarPacienteComponent implements OnInit{
   generos: Genero[] = [];
   carreras: Carrera[] = [];
+  carrerasFilter: Carrera[] = [];
   ubigeos: Ubigeo[] = [];
   condiciones: Condicion[] = [];
 
@@ -36,69 +39,20 @@ export class RegistrarPacienteComponent {
   departamento_seleccionado: string = '';
   provincia_seleccionada: string = '';
 
-  pacienteForm: FormGroup;
-  usuarioForm: FormGroup;
+
   personaForm: FormGroup;
   offset: number;
   isEdited: boolean = false;
   page: number;
 
-  onDepartamentoSeleccionado(event: Event) {
-    // Lógica adicional que quieras ejecutar cuando se selecciona un departamento
-    const target = event.target as HTMLSelectElement;
-    const value = target.value;
-    this.departamento_seleccionado = value;
-    console.log('Departamento seleccionado:', this.departamento_seleccionado);
-    this.loadProvincias();
-    // Por ejemplo, cargar provincias basadas en el departamento seleccionado
-  }
-
-  onProvinciaSeleccionada(event: Event) {
-    // Lógica adicional que quieras ejecutar cuando se selecciona una provincia
-    const target = event.target as HTMLSelectElement;
-    const value = target.value;
-    this.provincia_seleccionada = value;
-    console.log('Provincia seleccionada:', this.provincia_seleccionada);
-    this.loadDistritos();
-    // Por ejemplo, cargar distritos basados en el departamento y provincia seleccionados
-  }
-
-  // Suponiendo que tienes un método para manejar la selección de un distrito
-  onDistritoSeleccionado(departamento: string, provincia: string, distrito: string): void {
-    // Llamar a la API para obtener el id_ubigeo basado en el departamento, provincia y distrito seleccionados
-    this.pacienteService.getUbigeo(departamento, provincia, distrito).subscribe({
-      next: (response: any) => {
-        if ('data' in response && 'id_ubigeo' in response.data) {
-          const idUbigeo = response.data.id_ubigeo;
-          if (this.pacienteForm && this.pacienteForm.get('id_ubigeo')) {
-            this.pacienteForm.get('id_ubigeo')?.setValue(idUbigeo);
-          } else {
-            console.error('El formulario o el campo id_ubigeo no están disponibles');
-          }
-        } else {
-          console.error('La respuesta no contiene el campo id_ubigeo', response);
-        }
-      },
-      error: (error) => {
-        console.error('Error al obtener el id_ubigeo', error);
-      }
-    });
-  }
 
   constructor(
     @Inject(LOCALE_ID) public locale: string,
-    private pacienteService: PacienteService,
-    private authService: AuthService, private router: Router) {
+    private registrarPacienteService: RegistrarPacienteService ) {
     this.page = 1;
     this.offset = new Date().getTimezoneOffset();
 
-    this.usuarioForm = new FormGroup({
-      id_usuario: new FormControl('', []),
-      id_tipo_usuario: new FormControl('1', []),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      contrasenia: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    });
-
+   
     this.personaForm = new FormGroup({
       id_persona: new FormControl('', []),
       doc_identidad: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(9)]),
@@ -107,224 +61,29 @@ export class RegistrarPacienteComponent {
       fec_nacimiento: new FormControl('', [Validators.required]),
       id_genero: new FormControl('', [Validators.required]),
       num_telefono: new FormControl('', [Validators.required, Validators.minLength(7), Validators.maxLength(15)]),
-    });
+      id_tipo_usuario: new FormControl({ value: '1', disabled: true }), // Aquí se define el valor fijo y se deshabilita
+      email: new FormControl('', [Validators.required, Validators.email]),
+      contrasenia: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      id_ubigeo: new FormControl('', [Validators.required]),
 
-    this.pacienteForm = new FormGroup({
-      id_paciente: new FormControl('', []),
-      //id_ubigeo: new FormControl('', [Validators.required]),
       id_condicion: new FormControl('', [Validators.required]),
       id_carrera: new FormControl(''),
-      //id_persona: new FormControl('', [Validators.required]),
-      //id_usuario: new FormControl('', [Validators.required]),
-      id_ubigeo_departamento: new FormControl('', [Validators.required]),
-      id_ubigeo_provincia: new FormControl('', [Validators.required]),
-      id_ubigeo_distrito: new FormControl('', [Validators.required])
+
+      departamento_seleccionado: new FormControl(''),
+      provincia_seleccionada: new FormControl('')
     });
   }
-
-
 
   ngOnInit(): void {
-    this.getPacientes();
+    this.loadDepartamentos();
     this.loadCarreras();
     this.loadGeneros();
-    this.loadUbigeos();
     this.loadCondiciones();
-    this.loadDepartamentos();
-    this.loadProvincias();
-    this.loadDistritos();
   }
-
-  getPacientes(): void {
-    this.pacienteService.getPacientes().subscribe(
-      (result: any) => {
-        this.pacienteArray = result.data;
-      }, (err: any) => {
-        console.log(err);
-        Swal.close();
-        Swal.fire({
-          icon: 'error',
-          title: 'Advertencia ...',
-          text: 'Ah ocurrido un error!',
-        });
-      }
-    );
-  }
-
-
-  registrarPaciente(): void {
-    if (this.usuarioForm.valid && this.personaForm.valid && this.pacienteForm.valid) {
-      // Primero, registrar el usuario
-      this.pacienteService.registrarUsuario(this.usuarioForm.value).subscribe({
-        next: (resultadoUsuario: any) => {
-          // Aquí asumimos que el backend devuelve el ID del usuario creado
-          const usuarioResponse = resultadoUsuario as UsuarioResponse;
-          const idUsuario = resultadoUsuario.data.id_usuario;
-          // Ahora, establecer el ID del usuario en el formulario de la persona y registrar la persona
-          
-          this.pacienteService.registrarPersona(this.personaForm.value).subscribe({
-            next: (resultadoPersona: any) => {
-              // Similarmente, asumimos que el backend devuelve el ID de la persona creada
-              const personaResponse = resultadoPersona as PersonaResponse;
-              const idPersona = resultadoPersona.data.id_persona;
-              // Establecer el ID de la persona y el ID del usuario en el formulario del paciente
-              this.pacienteForm.get('id_persona')!.setValue(idPersona);
-              this.pacienteForm.get('id_usuario')!.setValue(idUsuario);
-              // Finalmente, registrar el paciente
-              this.pacienteService.registrarPaciente(this.pacienteForm.value).subscribe({
-                next: (resultadoPaciente) => {
-                  // Manejar la respuesta exitosa, por ejemplo, mostrando una alerta al usuario
-                  console.log('Paciente registrado con éxito', resultadoPaciente);
-                },
-                error: (error) => {
-                  // Manejar errores al registrar el paciente
-                  console.error('Error al registrar el paciente', error);
-                }
-              });
-            },
-            error: (error) => {
-              // Manejar errores al registrar la persona
-              console.error('Error al registrar la persona', error);
-            }
-          });
-        },
-        error: (error) => {
-          // Manejar errores al registrar el usuario
-          console.error('Error al registrar el usuario', error);
-        }
-      });
-    } else {
-      // Manejar la validación fallida de los formularios
-      console.error('Formularios no válidos');
-    }
-  }
-
-
-  /*registrarPaciente(): void {
-    console.log('test');
-    console.log(this.pacienteForm.value);
-    if (this.isEdited) {
-      this.actualizarPaciente();
-    } else {
-      this.pacienteService.registrarPaciente(this.pacienteForm.value).subscribe(
-        (result: any) => {
-          console.log(this.pacienteForm.value);
-          Swal.close();
-          Swal.fire({
-            icon: 'success',
-            title: 'registrarPaciente ...',
-            text: 'Se registró exitosamente los datos del paciente!',
-          });
-          this.pacienteForm.reset();
-          this.getPacientes();
-        },
-        (err: any) => {
-          console.log(this.pacienteForm.value);
-          console.log(err);
-          Swal.close();
-          Swal.fire({
-            icon: 'error',
-            title: 'Advertencia ...',
-            text: 'Ah ocurrido un error!',
-          });
-        }
-      );
-    }
-  }*/
-
-  editarPaciente(paciente: Paciente): void {
-    Swal.fire({
-      title: '¿Está seguro de editar este paciente?',
-      showCancelButton: true,
-      cancelButtonText: 'No',
-      confirmButtonText: 'Sí',
-      focusCancel: true,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.pacienteForm.setValue({
-          /*id_paciente: paciente.id_paciente,
-          doc_identidad: paciente.doc_identidad,
-          nombres: paciente.nombres,
-          apellidos: paciente.apellidos,
-          fec_nacimiento: formatDate(paciente.fec_nacimiento, 'yyyy-MM-dd', this.locale, 'UTC' + this.offset),
-          id_genero: paciente.id_genero,
-          email: paciente.email,
-          direccion: paciente.direccion,
-          num_telefono: paciente.num_telefono,
-          anio_ingreso: paciente.anio_ingreso,
-          id_carrera: paciente.id_carrera,
-          contrasenia: paciente.contrasenia*/
-        });
-        this.isEdited = true;
-      }
-    });
-  }
-
-  actualizarPaciente(): void {
-    const id = this.pacienteForm.value.id_paciente;
-
-    this.pacienteService.actualizarPaciente(id, this.pacienteForm.value).subscribe(
-      (result: any) => {
-        Swal.close();
-        Swal.fire({
-          icon: 'success',
-          title: 'actualizarPaciente ...',
-          text: '¡Se actualizó exitosamente el paciente!',
-        });
-        this.pacienteForm.reset();
-        this.getPacientes();
-        this.isEdited = false;
-      },
-      (err: any) => {
-        Swal.close();
-        Swal.fire({
-          icon: 'error',
-          title: 'Advertencia ...',
-          text: '¡Ha ocurrido un error al actualizar el paciente!',
-        });
-      }
-    );
-  }
-
-  eliminarPaciente(paciente: Paciente): void {
-    Swal.fire({
-      title: 'Esta seguro de eliminar la persona seleccionada?',
-      showCancelButton: true,
-      cancelButtonText: 'No',
-      confirmButtonText: 'Si',
-      focusCancel: true,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.pacienteService.eliminarPaciente(paciente.id_paciente).subscribe(
-          (reult: any) => {
-            console.log(paciente);
-            Swal.close();
-            Swal.fire({
-              icon: 'success',
-              title: 'eliminarPaciente ...',
-              text: 'Se elimino exitosamente al paciente!',
-            });
-            this.getPacientes();
-          },
-          (err: any) => {
-            console.log(paciente);
-            console.log(err);
-            Swal.close();
-            Swal.fire({
-              icon: 'error',
-              title: 'Advertencia ...',
-              text: 'Ah ocurrido un error al eliminar Paciente!',
-            });
-          }
-        );
-
-      }//end if
-    })//end then
-  }//end metodo
 
 
   loadGeneros() {
-    this.authService.getGeneros().subscribe(
+    this.registrarPacienteService.getGeneros().subscribe(
       (result: any) => {
         this.generos = result.data;
       },
@@ -334,56 +93,21 @@ export class RegistrarPacienteComponent {
     );
   }
 
-  loadUbigeos() {
-      this.authService.getUbigeos().subscribe(
-          (result: any) => {
-              this.ubigeos = result.data;
-          },
-          (err: any) => {
-              console.error('Error al cargar ubigeos', err);
-          }
-      );
-  }
-  
-  loadDepartamentos() {
-      this.authService.getDepartamentosUnicos().subscribe(
-          (result: any) => {
-              this.departamentos_filtrados = result.data;
-          },
-          (err: any) => {
-              console.error('Error al cargar departamentos', err);
-          }
-      );
-  }
-  
-  loadProvincias() {
-      // Asegúrate de tener una variable para el departamento seleccionado, por ejemplo:
-      // departamentoSeleccionado: string = 'Lima';
-      this.authService.getProvinciasUnicas(this.departamento_seleccionado).subscribe(
-          (result: any) => {
-              this.provincias_filtradas = result.data;
-          },
-          (err: any) => {
-              console.error('Error al cargar provincias', err);
-          }
-      );
-  }
-
-  loadDistritos() {
-    this.authService.getDistritosUnicos(this.departamento_seleccionado, this.provincia_seleccionada).subscribe(
-        (result: any) => {
-            this.distritos_filtrados = result.data;
-        },
-        (err: any) => {
-            console.error('Error al cargar distritos', err);
-        }
+  loadCondiciones() {
+    const idUbigeo =this.personaForm.get('id_ubigeo')?.value;
+    console.log("id_ ubigeo",idUbigeo)
+    this.registrarPacienteService.getCondiciones().subscribe(
+      (result: any) => {
+        this.condiciones = result.data;
+      },
+      (err: any) => {
+        console.error('Error al cargar condiciones', err);
+      }
     );
   }
 
-
-
   loadCarreras() {
-    this.authService.getCarreras().subscribe(
+    this.registrarPacienteService.getCarreras().subscribe(
       (result: any) => {
         this.carreras = result.data;
       },
@@ -393,17 +117,109 @@ export class RegistrarPacienteComponent {
     );
   }
 
-  loadCondiciones() {
-    this.authService.getCondiciones().subscribe(
+
+  loadDepartamentos() {
+    this.registrarPacienteService.getDepartamentos().subscribe(
       (result: any) => {
-        this.condiciones = result.data;
+        this.departamentos_filtrados = result.data;
       },
       (err: any) => {
-        console.error('Error al cargar condiciones', err);
+        console.error('Error al cargar departamentos', err);
       }
     );
   }
-  
 
-
+  onDepartamentoChange() {
+    const departamento = this.personaForm.get('departamento_seleccionado')?.value;
+    console.log('Departamento',departamento)
+    if (departamento) {
+      console.log()
+      this.registrarPacienteService.getProvincias(departamento).subscribe(
+        (result: any) => {
+          this.provincias_filtradas = result.data;
+          this.distritos_filtrados = []; // Reset distritos
+          this.personaForm.get('provincia_seleccionada')?.reset();
+          this.personaForm.get('id_ubigeo')?.reset();
+        },
+        (err: any) => {
+          console.error('Error al cargar provincias', err);
+        }
+      );
+    }
   }
+  
+  onProvinciaChange() {
+    const provincia = this.personaForm.get('provincia_seleccionada')?.value;
+    console.log('Provincia',provincia)
+    if (provincia) {
+      this.registrarPacienteService.getDistritos(provincia).subscribe(
+        (result: any) => {
+          this.distritos_filtrados = result.data;
+          console.log(this.distritos_filtrados)
+        },
+        (err: any) => {
+          console.error('Error al cargar distritos', err);
+        }
+      );
+    }
+  }
+
+  onCondicionChange() {
+    const condicion = this.personaForm.get('id_condicion')?.value;
+    if (condicion === '2') { // Estudiante universitario
+      this.registrarPacienteService.getCarreras().subscribe(
+        (result: any) => {
+          this.carrerasFilter = result.data;
+          console.log(this.carrerasFilter)
+          this.personaForm.get('id_carrera')?.setValidators([Validators.required]);
+          this.personaForm.get('id_carrera')?.updateValueAndValidity();
+        },
+        (err: any) => {
+          console.error('Error al cargar carreras', err);
+        }
+      );
+    } else {
+      this.carrerasFilter = [];
+      this.personaForm.get('id_carrera')?.clearValidators();
+      this.personaForm.get('id_carrera')?.updateValueAndValidity();
+    }
+    console.log(condicion)
+    const carrera = this.personaForm.get('id_carrera')
+    console.log("Carrera", carrera)
+  }
+
+  registrarPaciente(): void {
+    if (this.personaForm.valid) {
+    const formValue = { ...this.personaForm.value };
+    delete formValue.departamento_seleccionado;
+    delete formValue.provincia_seleccionada;
+    // Mostrar el contenido del formulario en la consola
+    console.log('Formulario a enviar:', formValue);
+
+    this.registrarPacienteService.registrarPaciente(formValue).subscribe(
+      (result: any) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Registro exitoso',
+          text: 'Se registró exitosamente el paciente',
+        });
+        this.personaForm.reset();
+      },
+      (err: any) => {
+        console.error(err);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Hubo un problema al registrar el paciente',
+        });
+      }
+    );
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Formulario incompleto',
+      text: 'Por favor, complete todos los campos requeridos',
+    });
+  }
+}
+}
